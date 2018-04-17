e887cc8b930e915077ebc9b5532847e5a4f4dcc5
smart <smart@appannie.com>
2018-04-17 10:46:37 +0800

    bug fix

--- data-collection/get_symbols_and_trigger_tasks.py
+++ data-collection/get_symbols_and_trigger_tasks.py
@@ -1,12 +1,17 @@
 import boto3
 
-sns = boto3.client('sns')
-symbol_list = ['mu', 'fb', 'yy', 'jd', 'momo', 'snap', 'wb', 'expe', 'ntes']
+
+def get_symbol_list():
+    s3 = boto3.client('s3')
+    obj = s3.get_object(Bucket='smart-quant-data', Key='symbol_list/symbol_list')
+    return obj['Body'].read().split('\n')
 
 
 def lambda_handler(event, context):
     # TODO implement
+    sns = boto3.client('sns')
     arn = 'arn:aws:sns:us-east-1:602379591537:smart-quant-symbol'
+    symbol_list = filter(None, get_symbol_list())
     for symbol in symbol_list:
         sns.publish(
             TargetArn=arn,
